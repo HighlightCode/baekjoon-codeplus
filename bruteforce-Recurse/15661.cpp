1f354d1813788faddb00f964b240b434c55c490e
Parksangin <51946218+Justdoit19@users.noreply.github.com>
2022-03-29 18:25:26 +0900

    add some

--- bruteforce-Recurse/15661.cpp
+++ bruteforce-Recurse/15661.cpp
@@ -1,80 +1,52 @@
-#include<iostream>
+#include <iostream>
 #include <vector>
-#include <limits.h>
-#include<algorithm>
-
 using namespace std;
-
-
-int calculate(vector<bool>& s_l, vector<vector<int>>& point) {
-	int s_point = 0;
-	int l_point = 0;
-	vector<int> s_p;
-	vector<int> l_p;
-
-	for (int i = 0; i < s_l.size(); i++) {
-		if (s_l[i] == true)
-			s_p.push_back(i);
-		else
-			l_p.push_back(i);
-	}
-
-	for (int i = 0; i < s_p.size(); i++) {
-		int x = s_p[i];
-		for (int j = 0; j < s_p.size(); j++) {
-			int y = s_p[j];
-			s_point += point[x][y];
-		}
-	}
-
-	for (int i = 0; i < l_p.size(); i++) {
-		int x = l_p[i];
-		for (int j = 0; j < l_p.size(); j++) {
-			int y = l_p[j];
-			l_point += point[x][y];
-		}
-	}
-	return abs(s_point - l_point);
+int n;
+int arr[21][21];
+int back(int index, vector<int>& first, vector<int>& second) {
+    if (index == n + 1) {
+        if (first.size() < 1) return -1;
+        if (second.size() < 1) return -1;
+        int t1 = 0;
+        int t2 = 0;
+        for (int i = 0; i < first.size(); i++) {
+            for (int j = 0; j < first.size(); j++) {
+                if (i == j) continue;
+                t1 += arr[first[i]][first[j]];
+            }
+        }
+        for (int i = 0; i < second.size(); i++) {
+            for (int j = 0; j < second.size(); j++) {
+                if (i == j) continue;
+                t2 += arr[second[i]][second[j]];
+            }
+        }
+        int diff = abs(t1 - t2);
+        return diff;
+    }
+    int ans = -1;
+    first.push_back(index);
+    int x = back(index + 1, first, second);
+    if (ans == -1 || (x != -1 && x < ans)) {
+        ans = x;
+    }
+    first.pop_back();
+    second.push_back(index);
+    int y = back(index + 1, first, second);
+    if (ans == -1 || (y != -1 && y < ans)) {
+        ans = y;
+    }
+    second.pop_back();
+    return ans;
 }
-
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
-
-	int N;
-	cin >> N;
-
-	vector<bool> s_l;
-	vector<vector<int>> point_sl;
-	int result = INT_MAX;
-
-	for (int i = 0; i < N; i++) {
-		vector<int> tmp;
-		for (int j = 0; j < N; j++) {
-			int k;
-			cin >> k;
-			tmp.push_back(k);
-		}
-		point_sl.push_back(tmp);
-	}
-
-	for (int i = 0; i < N; i++) {
-		if (i < N / 2)
-			s_l.push_back(0);
-		else
-			s_l.push_back(1);
-	}
-
-	while (true) {
-		if (next_permutation(s_l.begin(), s_l.end())) {
-			result = min(result, calculate(s_l, point_sl));
-		}
-		else {
-			break;
-		}
-	}
-
-	cout << result;
-	return 0;
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cin >> arr[i][j];
+        }
+    }
+    vector<int> first, second;
+    cout << back(1, first, second);
 }
