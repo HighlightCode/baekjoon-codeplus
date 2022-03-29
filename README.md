550a9f12423367f99b3465e8072d7f221bb2a98b
Parksangin <51946218+Justdoit19@users.noreply.github.com>
2022-03-29 13:51:51 +0900

    add readme

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
-# BAEK JOON CODE TUTORIAL SOLUTION
+﻿# BAEK JOON CODE TUTORIAL SOLUTION
 
 - solution for the baek joon code plus ..
 - https://www.acmicpc.net/workbook/codeplus
+
+# BOJ 코드 플러스 문제집 solution 입니다.
+
+[BOJ 코드 플러스 문제집](https://www.acmicpc.net/workbook/codeplus)의 문제에 대한 풀이 코드입니다.
+문제별 난이도 표시
+🐻 : 브론즈 [BRONZE]
+🐭 : 실버 [SILVER]
+🐱 : 골드 [GOLD]
+🦕 : 플래티넘 [PLATINUM]
+🐳 : 다이아 [DIAMOND]
+
+| 번호                                                            | 제목                  | C++ | solved tier |
+| --------------------------------------------------------------- | --------------------- | --- | ----------- |
+| [수학](https://www.acmicpc.net/workbook/view/9370)              | 코딩테스트 준비-기초  | [x] |             |
+| [브루트포스](https://www.acmicpc.net/workbook/view/9371)        | 코딩테스트 준비-기초  | [x] |             |
+| [브루트포스 - N과 M](https://www.acmicpc.net/workbook/view/9372)| 코딩테스트 준비-기초  | [x] |             |
+| [브루트포스 - 재귀](https://www.acmicpc.net/workbook/view/9373) | 코딩테스트 준비-기초  | [x] |             |
+| [브루트포스 - 순열](https://www.acmicpc.net/workbook/view/9374) | 코딩테스트 준비-기초  | [x] |             |
---
+++ bruteforce-Recurse/15661.cpp
@@ -0,0 +1,80 @@
+#include<iostream>
+#include <vector>
+#include <limits.h>
+#include<algorithm>
+
+using namespace std;
+
+
+int calculate(vector<bool>& s_l, vector<vector<int>>& point) {
+	int s_point = 0;
+	int l_point = 0;
+	vector<int> s_p;
+	vector<int> l_p;
+
+	for (int i = 0; i < s_l.size(); i++) {
+		if (s_l[i] == true)
+			s_p.push_back(i);
+		else
+			l_p.push_back(i);
+	}
+
+	for (int i = 0; i < s_p.size(); i++) {
+		int x = s_p[i];
+		for (int j = 0; j < s_p.size(); j++) {
+			int y = s_p[j];
+			s_point += point[x][y];
+		}
+	}
+
+	for (int i = 0; i < l_p.size(); i++) {
+		int x = l_p[i];
+		for (int j = 0; j < l_p.size(); j++) {
+			int y = l_p[j];
+			l_point += point[x][y];
+		}
+	}
+	return abs(s_point - l_point);
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int N;
+	cin >> N;
+
+	vector<bool> s_l;
+	vector<vector<int>> point_sl;
+	int result = INT_MAX;
+
+	for (int i = 0; i < N; i++) {
+		vector<int> tmp;
+		for (int j = 0; j < N; j++) {
+			int k;
+			cin >> k;
+			tmp.push_back(k);
+		}
+		point_sl.push_back(tmp);
+	}
+
+	for (int i = 0; i < N; i++) {
+		if (i < N / 2)
+			s_l.push_back(0);
+		else
+			s_l.push_back(1);
+	}
+
+	while (true) {
+		if (next_permutation(s_l.begin(), s_l.end())) {
+			result = min(result, calculate(s_l, point_sl));
+		}
+		else {
+			break;
+		}
+	}
+
+	cout << result;
+	return 0;
+}
